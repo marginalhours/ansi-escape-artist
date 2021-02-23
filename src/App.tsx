7685e701a3c3f4267cc8eec65c4a5a420546eb4f
Tom Keefe <8655118+MisterKeefe@users.noreply.github.com>
2021-02-23 08:48:31 +0000

    stubbing out

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,30 @@
 import React, { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+
+import Picker from './Picker';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header text-gray-500">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+    <div className='App'>
+      <header className='App-header text-gray-500'>
       </header>
+      <main>
+        <select>
+          <option value='-1'>Select a preset...</option>
+        </select>
+        <input type='text' placeholder='Enter your text...'></input>
+        <label>Foreground:</label> <Picker></Picker>
+        <label>Background:</label> <Picker></Picker>
+        <label>Bold</label><input type='checkbox'></input>
+        <label>Underline</label><input type='checkbox'></input>
+        <output>Sequence</output>
+        <output>Preview</output>
+      </main>
+      <footer></footer>
     </div>
   )
-}
+};
 
-export default App
+export default App;
---
+++ src/Picker.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+function Picker () { 
+    return (<div>
+        Picker
+    </div>)
+}
+
+export default Picker;
